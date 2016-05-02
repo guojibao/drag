3c57e0270c6fb1ae832b8866f4888056cfd190fb
guojibao <768081262@qq.com>
2016-05-02 12:01:29 +0800

    success drag

--- drag.js
+++ drag.js
@@ -12,7 +12,12 @@ function drag(id){
         document.onmousemove = function(ev){
             obj.style.left = ev.pageX - disX + "px";
             obj.style.top = ev.pageY -disY + "px";
-        }
-    };
+        };
+        document.onmouseup = function(){
+            document.onmousemove = null;
+            document.onmouseup = null;
+        };
+    }
+    return false;
 
 }
